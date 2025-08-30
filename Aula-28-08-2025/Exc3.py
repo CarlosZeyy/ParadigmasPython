081b078a86d7399011ac13024bd7e6d2456fa1c5
Carlos Moises <carloszeeyy@gmail.com>
2025-08-30 17:06:43 -0300

    Fix: Exc3 estava sem check no tamanho da senha e sem verificar a senha mininuscula

--- Aula-28-08-2025/Exc3.py
+++ Aula-28-08-2025/Exc3.py
@@ -1,17 +1,10 @@
-check = ""
-password = "INFINITY"
+while True :
+    password = str(input("Digite a senha: "))
 
-while check != password :
-    check = str(input("Digite a senha: "))
+    if len(password) < 8 :
+        print("Senha deve conter 8 caracteres")
 
-print("Você acertou a senha")
-
-# * 2° Metodo
-
-# while True :
-#     password = str(input("Digite a senha: "))
-#     if password == "INFINITY":
-#         print("Você digitou a senha certa!")
-#         break
+    if password == "INFINITY" or password == "infinity":
+        print("Você digitou a senha certa!")
+        break
     
-#     print("Você digitou: ", password)
