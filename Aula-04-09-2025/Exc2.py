95a4b78458fe546ae758e13d8d7f2478061165b4
Carlos Moises <carloszeeyy@gmail.com>
2025-09-04 21:16:57 -0300

    Refactor: improve readability by restructuring temperature and humidity conditions

--- Aula-04-09-2025/Exc2.py
+++ Aula-04-09-2025/Exc2.py
@@ -1,11 +1,13 @@
 temp = float(input("Digite o valor da temperatura: "))
 umidade = int(input("Digite o valor da umidade: "))
 
-if temp > 25 and umidade > 60 :
-    print("Ar quente e úmido")
-elif temp > 25 and umidade < 60 :
-    print("Ar quente e seco")
-elif temp <= 25 and umidade > 60 :
-    print("Ar frio e úmido")
-elif temp <= 25 and umidade < 60 :
-    print("Ar frio e seco")
+if temp > 25: 
+    if umidade > 60 :
+        print("Ar quente e úmido")
+    elif umidade < 60 :
+        print("Ar quente e seco")
+else :
+    if umidade > 60 :
+        print("Ar frio e úmido")
+    elif umidade < 60 :
+        print("Ar frio e seco")
