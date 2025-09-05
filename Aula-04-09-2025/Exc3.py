508338e166385a4f6adc7327a090c59f75faf38f
Carlos Moises <carloszeeyy@gmail.com>
2025-09-05 00:15:06 -0300

    Refactor: streamline name input process using a loop

--- Aula-04-09-2025/Exc3.py
+++ Aula-04-09-2025/Exc3.py
@@ -1,16 +1,8 @@
 nomes = []
 
-nome1 = input("Digite o primeiro nome: ")
-nome2 = input("Digite o segundo nome: ")
-nome3 = input("Digite o terceiro nome: ")
-nome4 = input("Digite o quarto nome: ")
-nome5 = input("Digite o quinto nome: ")
-
-nomes.append(nome1)
-nomes.append(nome2)
-nomes.append(nome3)
-nomes.append(nome4)
-nomes.append(nome5)
+for i in range(5) :
+    nome = input(f"Digite o {i + 1}° nome: ")
+    nomes.append(nome)
 
 print(f"Lista de nomes: {nomes}")
 
