b731a8ad06579afe2ad41940030891e6a294933c
Carlos Moises <carloszeeyy@gmail.com>
2025-09-05 09:59:11 -0300

    Refactor: adicionando a funcionalidade de quantidade de nomes de forma dinamica ao inves do valor fixo (5) proposto no exercicio.

--- Aula-04-09-2025/Exc3.py
+++ Aula-04-09-2025/Exc3.py
@@ -1,6 +1,7 @@
 nomes = []
+quantidadeNomes = int(input("Digite a quantidade de nomes que vai digitar: "))
 
-for i in range(5) :
+for i in range(quantidadeNomes) :
     nome = input(f"Digite o {i + 1}° nome: ")
     nomes.append(nome)
 
