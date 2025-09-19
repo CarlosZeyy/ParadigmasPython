f416b527a1efe7c2a51c1aad4658ad47a1d98d86
Carlos Moises <carloszeeyy@gmail.com>
2025-09-19 00:29:30 -0300

    Fix: Verificação do numero par

--- Aula-18-09-2025/Exc03.py
+++ Aula-18-09-2025/Exc03.py
@@ -2,7 +2,7 @@ numero = int(input('Digite um numero: '))
 
 if numero < 0:
     print('Negativo')
-elif (numero % 2 == 0) > 0 or numero == 0 :
+elif numero % 2 == 0 :
     print('Par')
 else: 
     print('Impar')
